04dccbff76c32c588898db589db853a19fbd6ef7
Brian Harvey <brian.harv3y@gmail.com>
2020-08-12 16:56:32 -0700

    simplify js for preseting all caps

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     const userinput = $("input#userinput").val();
-    const userInputUpper = userinput.toUpperCase();
-    $(".userinput").text(userInputUpper); 
+//    const userInputUpper = userinput.toUpperCase();
+    $(".userinput").text(userinput.toUpperCase()); 
 
     $("#return").show();
   });
